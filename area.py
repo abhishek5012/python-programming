78069bd87521c34cd0a66e002e101ea87893dbad
abhishek5012 <90345464+abhishek5012@users.noreply.github.com>
2021-12-13 10:10:11 -0800

    Create area.py

---
+++ area.py
@@ -0,0 +1,4 @@
+r=float(input("ENTER THE VALUE OF THE RADIUS (in cms) :"))
+s=3.141592653*r**2
+print("the radius of the circle is in  :",r,"cm")            
+print("THE area of the circle is in :",s,"cm^2")    
